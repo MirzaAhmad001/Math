719711a7ca662ea4cb585fc5fe203da25e028e97
MirzaAhmad <mirzaahmadsodiqov.001@gmail.com>
2024-07-10 17:43:43 +0500

    EKUK added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Enter ");
+    }
 
-
-
-
+    int EKUK(int a, int b) {
+        int x = 0;
+        while (true) {
+            x ++;
+            if (x % a == 0 && x % b == 0) {
+                return x;
+            }
+        }
     }
 }
