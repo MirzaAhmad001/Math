aecc2fbecf718e8170c6b7518051fa019341c103
Zarif <zarifabdullox@gmail.com>
2024-07-10 17:29:49 +0500

    Ecubni added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
-
-
-
+        
     }
+    private static int gCD(int a, int b){
+        int c = 0;
+        if (a > b){
+            c = b;
+        }else {
+            c = a;
+        }
+        for (int i = c / 2; i > 0; i --) {
+            if (a % i == 0 && b % i == 0){
+                return i;
+            }
+        }
+    }
+
 }
