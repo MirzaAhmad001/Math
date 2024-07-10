f6aed99afedd00a626cf13a0c35707005c921cae
MuxammadaminErkinboev <muxammadaminerkinboev6899@gmail.com>
2024-07-10 17:42:08 +0500

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
+    }
 
+    public static void primeNumbersTillN(int n){
+        for(int i = 2; i <= n; i++){
+            boolean isPrime = true;
+            for(int j = 2; j <= Math.sqrt(i); j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(i);
+            }
+        }
+    }
 
-
+    public static void fibonacciSeriesTillN(int n){
+        int first = 0;
+        int second = 1;
+        System.out.print(first + " " + second + " ");
+        for(int i = 2; i < n; i++){
+            int next = first + second;
+            System.out.print(next + " ");
+            first = second;
+            second = next;
+        }
     }
 }
